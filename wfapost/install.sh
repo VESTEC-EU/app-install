f49c639f14a0edee7bf7dc3e794a99c254b2185d
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-07-10 10:14:34 +0100

    fix python gdal module link issues

--- wfapost/install.sh
+++ wfapost/install.sh
@@ -7,11 +7,13 @@ set -e
 thisdir=$(readlink -f $(dirname $BASH_SOURCE))
 . $thisdir/env.sh
 
-python3 -m venv --system-site-packages $wfapost_install_dir
+if [ ! -d $wfapost_install_dir/bin/ ]; then 
+    python3 -m venv --system-site-packages $wfapost_install_dir
+fi
 . $wfapost_install_dir/bin/activate
 
 # GDAL extensions must properly link agains libgdal.so
-pip3 install --global-option=build_ext --global-option=-R$GDAL_DIR/lib GDAL==$wfapost_gdal_version
+pip3 install --global-option=build_ext --global-option=-R$GDAL_DIR/lib:$GCC_PATH/snos/lib64 GDAL==$wfapost_gdal_version
 
 # Rest as normal
 pushd wfapost
