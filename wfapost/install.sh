2811780837d3078dd95de473956cb36d9f272294
Rupert Nash <r.nash@epcc.ed.ac.uk>
2020-11-23 21:56:55 +0000

    wfa post proc

--- wfapost/install.sh
+++ wfapost/install.sh
@@ -7,14 +7,16 @@ set -e
 thisdir=$(readlink -f $(dirname $BASH_SOURCE))
 . $thisdir/env.sh
 
-if [ ! -d $wfapost_install_dir/bin/ ]; then 
-    python3 -m venv --system-site-packages $wfapost_install_dir
+if [ ! -d $prefix/bin/ ]; then 
+    python3 -m venv --system-site-packages $prefix
 fi
-. $wfapost_install_dir/bin/activate
+. $prefix/bin/activate
 
 # GDAL extensions must properly link agains libgdal.so
-pip3 install --global-option=build_ext --global-option=-R$GDAL_DIR/lib:$GCC_PATH/snos/lib64 GDAL==$wfapost_gdal_version
+pip3 install --global-option=build_ext --global-option=-R$GDAL_DIR/lib GDAL==$wfapost_gdal_version
 
 # Rest as normal
 pushd wfapost
 pip3 install -r requirements.txt .
+
+chmod -R g+w $prefix
--- wfapost/modtemplate.tcl
+++ wfapost/modtemplate.tcl
@@ -3,6 +3,6 @@ module-whatis "WFA post processing script"
 
 module load python/3.8.3
 
-setenv WFAPOST_DIR "${wfapost_install_dir}"
-setenv VIRTUAL_ENV "${wfapost_install_dir}"
-prepend-path PATH "${wfapost_install_dir}/bin"
+setenv WFAPOST_DIR "${prefix}"
+setenv VIRTUAL_ENV "${prefix}"
+prepend-path PATH "${prefix}/bin"
