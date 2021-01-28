80135eef2de6b9ca4c42cd8844fe68bfe0011a7c
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-01-28 15:21:16 +0000

    conan

--- packages/conan/env.sh
+++ packages/conan/env.sh
@@ -1,16 +1,13 @@
 if [ -z "$INSTALLER_CONAN_ENV_SH" ]; then
     INSTALLER_CONAN_ENV_SH=1
-
-    export installer_conan_dir=$(readlink -f $(dirname $BASH_SOURCE))
+    installer_conan_dir=$(readlink -f $(dirname $BASH_SOURCE))
     . $installer_conan_dir/../../env.sh
 
-    # Open source, install world usable
-    export app_dir=$pub_app_dir
-    export mod_dir=$pub_mod_dir
+    name=conan
+    version=1.31.3
+    visibility=public
 
-    export name=conan
-    export version=1.31.3
-    export prefix=$app_dir/$name/$version
+    installer_init
 
     module load python/3.8.5
 fi
