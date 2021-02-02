3fa85cfc34e843e8f0aa2daa87d011b0f73628c6
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-02-02 16:38:01 +0000

    wfapost

--- packages/wfapost/install.sh
+++ packages/wfapost/install.sh
@@ -3,7 +3,7 @@ if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
     echo "Don't source me"
     return 1
 fi
-set -e
+
 thisdir=$(readlink -f $(dirname $BASH_SOURCE))
 . $thisdir/env.sh
 
---
+++ platforms/archer2/wfapost/config.sh
@@ -0,0 +1,2 @@
+module load python/3.8.5
+module load gdal/$wfapost_gdal_version
