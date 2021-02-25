a9de85c051fc1781d62a0d00941f22a1a6e6935b
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-02-25 19:25:08 +0000

    fix node linking so as not to require openssl module loaded at runtime

--- platforms/archer2/nodejs/config.sh
+++ platforms/archer2/nodejs/config.sh
@@ -1,4 +1,6 @@
 module load gcc/10.1.0
 module load openssl/1.1.1d
 export CC=gcc
+export LD_RUN_PATH=$OPENSSL_DIR/lib
+export LDFLAGS=-Wl,--enable-new-dtags
 configure_other_args="--shared-openssl"
