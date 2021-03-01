591e695cb129db6093d11b2c28ed33811ade667f
Riccardo Fellegara <riccardo.fellegara@dlr.de>
2021-03-01 11:14:44 +0000

    Update paraview to v5.9 and provide patch for archer2 for its superbuild script

--- paraview/install_pv.sh
+++ paraview/install_pv.sh
@@ -5,7 +5,7 @@ if [ ! -d paraview-source ]; then
   git clone https://gitlab.kitware.com/paraview/paraview-superbuild.git paraview-source
   mkdir paraview-build
   cd paraview-source
-  git checkout e9736b30ce3e50217e113080d1eeedf504c764b1
+  git checkout v5.9.0
   git submodule update --init
   cd ..
 fi
@@ -14,23 +14,27 @@ if [ ! -d paraview-build ]; then
   mkdir paraview-build
 fi
 
+#NOTICE: OSPRAY IS DISABLED
+
 cd paraview-build
 cmake -DSUPERBUILD_PROJECT_PARALLELISM=8 \
       -DPARAVIEW_BUILD_EDITION=CATALYST_RENDERING \
-      -Dparaview_SOURCE_SELECTION=5.8.1-RC1 \
+      -Dparaview_SOURCE_SELECTION=5.9.0 \
       -DENABLE_python=ON \
       -DENABLE_python3=ON \
       -DENABLE_paraviewweb=ON \
       -DENABLE_osmesa=ON \
       -Dmesa_USE_SWR=OFF \
-      -DPARAVIEW_USE_MPI=On \
       -DENABLE_mpi=On \
-      -DENABLE_ospray=ON \
-      -DUSE_SYSTEM_MPI=On \
-      -DCMAKE_INSTALL_PREFIX=../install/paraview-5.8.1 \
+      -DENABLE_ospray=Off \
+      -DUSE_SYSTEM_mpi=On \
+      -DENABLE_vtkm=OFF \
+      -DCMAKE_INSTALL_PREFIX=../install/paraview-5.9.0 \
       -DCMAKE_INSTALL_RPATH="\$ORIGIN" \
       ../paraview-source
 
 make
 make install
 
+# since the superbuild final install will fail, we need to manually copy the lib folder in a way that ipicmini can see it
+cp -r paraview-build/install/lib/* install/ipicmini/lib/
