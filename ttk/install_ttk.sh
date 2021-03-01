c4936cdfb1d97805eed4da096d8affd887f5716c
Riccardo Fellegara <riccardo.fellegara@dlr.de>
2021-03-01 11:17:12 +0000

    Update ttk script to point to the new paraview and to the system boost library in archer2

--- ttk/install_ttk.sh
+++ ttk/install_ttk.sh
@@ -8,7 +8,9 @@ if [ ! -d ttk-source ]; then
   mkdir ttk-build
   cd ttk-source
   #git checkout a26adce85a53a7581dfd4fc7bef96305db8a9383
-  git checkout 265f613646bda66e1b43f2a300dfa100b255c466
+  #git checkout 265f613646bda66e1b43f2a300dfa100b255c466
+  # the following checkout points to TTK-v0.9.9
+  git checkout 15fd3ecd820a7ed8c5dd5c984ce3f9c5a09b7e46
   rm -rf paraview/WRLExporter/ core/vtk/ttkWRLExporter/ 
   cd ..
 fi
@@ -19,12 +21,13 @@ if [ ! -d ttk-build ]; then
 fi
 
 cd ttk-build
-cmake -DParaView_DIR=$root/paraview-build/install/lib/cmake/paraview-5.8\
+cmake -DParaView_DIR=$root/paraview-build/install/lib/cmake/paraview-5.9\
       -DTTK_ENABLE_KAMIKAZE=ON \
-      -DTTK_INSTALL_PLUGIN_DIR=$root/install/paraview-5.8.1/lib/paraview-5.8/plugins \
+      -DTTK_INSTALL_PLUGIN_DIR=$root/install/paraview-5.9/lib/paraview-5.9/plugins \
       -DTTK_BUILD_STANDALONE_APPS=OFF \
       -DVTK_MODULE_ENABLE_ttkCinemaImaging=DONT_WANT \
       -DVTK_MODULE_ENABLE_ttkUserInterfaceBase=DONT_WANT \
+      -DBoost_INCLUDE_DIR=/work/y07/shared/libs/boost/1.72.0/CRAYCLANG/10.0/include \
       -DCMAKE_INSTALL_PREFIX=$root/install/ttk/ \
       $root/ttk-source
 
