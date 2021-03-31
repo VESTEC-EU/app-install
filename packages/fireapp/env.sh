8d423ef0783dcec3d47dcbf7c6d0e2825592ae3a
Charles Gueunet <charles.gueunet@kitware.com>
2021-03-31 13:20:43 +0100

    Update WFA to use the new ParaView module

--- packages/fireapp/env.sh
+++ packages/fireapp/env.sh
@@ -19,4 +19,6 @@ if [ -z "$INSTALLER_FIREAPP_ENV_SH" ]; then
     cmake_vars[FIREAPP_CONAN]=ON
     cmake_vars[FIREAPP_CONAN_GDAL]=OFF
     cmake_vars[CONAN_CMD]=$CONAN_DIR/bin/conan
+    cmake_vars[USE_CATALYST]=ON
+    cmake_vars[ParaView_DIR]=$PARAVIEW_DIR/lib/cmake/paraview-$PARAVIEW_VERSION
 fi
