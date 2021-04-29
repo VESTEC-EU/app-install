3d85d8af64a03a2b9e0f6b4a9c6a99043398e82a
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-04-29 09:50:31 +0100

    make fireapp build work with PV

--- platforms/cirrus/fireapp/config.sh
+++ platforms/cirrus/fireapp/config.sh
@@ -1,7 +1,10 @@
 module load gcc/6.3.0
+export CC=$(which gcc)
 module load cmake/3.17.3
 module load netcdf/4.7.4
 module load gdal/3.1.0
 module load conan/1.31.3
 module load mpt/2.22
+module load paraview/5.9.0
+module load python/3.8.5
 cmake_vars[MPI_CXX_LINK_FLAGS]=-Wl,-rpath=$MPI_ROOT/lib
