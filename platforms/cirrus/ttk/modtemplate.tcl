d42f7d78fa647f26d081937f0bdba143e0a382a8
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-06-01 13:22:01 +0100

    ensure ttk load boost at runtime

--- platforms/cirrus/ttk/modtemplate.tcl
+++ platforms/cirrus/ttk/modtemplate.tcl
@@ -3,3 +3,7 @@ module-whatis "TTK version ${version} including Paraview ${pv_version}"
 
 setenv TTK_DIR "${prefix}"
 prepend-path PATH "${prefix}/bin"
+
+if {![is-loaded boost]} {
+    module load boost/1.73.0
+}
