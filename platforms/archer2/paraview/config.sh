df9b7b0159bd733c158d25af7805147d47f83379
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-06-22 12:15:52 +0100

    correct archer2 specific paraview config

--- platforms/archer2/paraview/config.sh
+++ platforms/archer2/paraview/config.sh
@@ -6,4 +6,7 @@ mod_use_idem $priv_mod_dir
 
 module load cmake/3.18.4
 module load gcc/10.1.0
-
+module load python/3.8.5
+module load boost/1.72.0
+# To let CMake find boost no bother
+export Boost_ROOT=$BOOST_DIR
