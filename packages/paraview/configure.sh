afd74fc26426fba53a5680c7239f3fa3ac5be26b
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-08-12 11:46:52 +0100

    further paraview

--- packages/paraview/configure.sh
+++ packages/paraview/configure.sh
@@ -1,1 +1,19 @@
-../../generic/cmake/configure.sh
+#!/bin/bash
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    echo "Don't source me"
+    return 1
+fi
+set -e
+
+thisdir=$(readlink -f $(dirname $BASH_SOURCE))
+. $thisdir/env.sh
+
+# Set up the virtual env first
+if [ ! -f $prefix/bin/activate ]; then 
+    python3 -m venv --system-site-packages $prefix
+fi
+. $prefix/bin/activate
+
+pip3 install numpy==${numpy_version}
+
+$thisdir/main-configure.sh
