bdd481e195381e06ea0f6abb272faab488e1f4be
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-08-12 15:13:03 +0100

    update TTK to use paraview module

--- platforms/cirrus/ttk/config.sh
+++ platforms/cirrus/ttk/config.sh
@@ -3,3 +3,6 @@ module load gcc/6.3.0
 module load python/3.8.5
 module load boost/1.73.0
 module load eigen/3.3.9
+export Eigen3_ROOT=$EIGEN_DIR
+# Ensure that TTK and its python modules are installed consistently
+cmake_vars[CMAKE_INSTALL_LIBDIR]=lib
