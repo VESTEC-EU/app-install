4ebbe9615e3950710a52a907df1d6f56fb16c31d
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-08-20 13:41:24 +0100

    Tweak fire sim build to work with TTK

--- packages/fireapp/modtemplate.tcl
+++ packages/fireapp/modtemplate.tcl
@@ -1,5 +1,8 @@
 #%Module
 module-whatis "Wildfire Analyst VESTEC app"
 
+if {![is-loaded ttk]} {
+    module load ttk/0.9.9
+}
 setenv FIREAPP_DIR "${prefix}"
 prepend-path PATH "${prefix}/bin"
--- platforms/cirrus/fireapp/config.sh
+++ platforms/cirrus/fireapp/config.sh
@@ -5,6 +5,6 @@ module load netcdf/4.7.4
 module load gdal/3.1.0
 module load conan/1.31.3
 module load mpt/2.22
-module load paraview/5.9.0
-module load python/3.8.5
+module load ttk/0.9.9
+
 cmake_vars[MPI_CXX_LINK_FLAGS]=-Wl,-rpath=$MPI_ROOT/lib
