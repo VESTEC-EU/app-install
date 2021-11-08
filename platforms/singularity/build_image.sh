28fa0650afe5bb22ee8e3f62f989a5d098a18141
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-11-08 12:00:44 +0000

    fixup: sudo sing build

--- platforms/singularity/build_image.sh
+++ platforms/singularity/build_image.sh
@@ -2,5 +2,8 @@
 
 pkg_name=$1
 
-singularity build cwl.sif platforms/singularity/$pkg_name/Singularity
+# Sudo only looks for binaries in a few specified dirs, provide full
+# path instead (as GH CI doens't put it in /usr/bin or whatever).
+singularity_exe=`which singularity`
+sudo $singularity_exe build cwl.sif platforms/singularity/$pkg_name/Singularity
 
