76be7deff3e53b41f57d032351ef355cd10f7a65
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-11-08 12:15:17 +0000

    think better about paths

--- platforms/singularity/build_image.sh
+++ platforms/singularity/build_image.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+thisdir=$(readlink -f $(dirname $BASH_SOURCE))
 
 pkg_name=$1
 
 # Sudo only looks for binaries in a few specified dirs, provide full
 # path instead (as GH CI doens't put it in /usr/bin or whatever).
 singularity_exe=`which singularity`
-sudo $singularity_exe build cwl.sif platforms/singularity/$pkg_name/Singularity
+
+cd $thisdir/$pkg_name
+
+sudo $singularity_exe build cwl.sif Singularity
 
